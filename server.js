be582cda5ac91df0029792ffc82f1dc9b25b5147
MkumarMonu <148427409+MkumarMonu@users.noreply.github.com>
2025-06-16 17:29:33 +0530

    cors error

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: allowedOrigins,
     methods: ["GET", "POST", "DELETE", "PUT"],
   },
 });
