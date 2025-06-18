2e7f34a16dd4afb54d797c97884431ea2edb8a5d
MkumarMonu <148427409+MkumarMonu@users.noreply.github.com>
2025-06-18 12:26:37 +0530

    error resolve

--- src/routes/chat.route.js
+++ src/routes/chat.route.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { getChat } from "../controllers/chat.controller.js";
+import { authenticateUser } from "../middlewares/authenticateUser.js";
 
 const chatRouter = express.Router();
-chatRouter.route("/chatHistory/:targetUserId").get(getChat);
+chatRouter.route("/chatHistory/:targetUserId").get(authenticateUser, getChat);
 
 export { chatRouter };
